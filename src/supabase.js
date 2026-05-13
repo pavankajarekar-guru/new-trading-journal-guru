cf74775f6706d02532b31177f449b4abf56dea04
pavankajarekar-guru <pavankajarekar@gmail.com>
2026-05-13 17:44:29 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'PASTE_YOUR_SUPABASE_URL'
-const supabaseKey = 'PASTE_YOUR_ANON_KEY'
+const supabaseUrl = 'https://lziqfwxtibrikhzilnbu.supabase.co/rest/v1/'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6aXFmd3h0aWJyaWtoemlsbmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjkyNzcsImV4cCI6MjA5NDI0NTI3N30.wHzeJ2UawmuBdnkyCoutoxn7Wcoa52AV03QmJhoO9FM'
 
 export const supabase = createClient(
   supabaseUrl,
