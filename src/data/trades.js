be22eb4b13fefdb0b544d8ec41915caaffef39ef
pavankajarekar-guru <pavankajarekar@gmail.com>
2026-05-13 16:37:14 +0530

    Create trades.js

---
+++ src/data/trades.js
@@ -0,0 +1,15 @@
+export const sampleTrades = [
+  {
+    pair: 'EUR/USD',
+    entry: '1.0840',
+    exit: '1.0890',
+    profit: '+250'
+  },
+
+  {
+    pair: 'BTC/USD',
+    entry: '62000',
+    exit: '64000',
+    profit: '+400'
+  }
+]
