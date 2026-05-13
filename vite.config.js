64aec4b807dd3f2112f076a79fa8d5325bc4b3ef
pavankajarekar-guru <pavankajarekar@gmail.com>
2026-05-13 17:37:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Trading Journal Guru',
+        short_name: 'TJGuru',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone'
+      }
+    })
+
+  ]
 })
