d3fc0e2f0c0738ad63c31511583b88713072fe7a
pavankajarekar-guru <pavankajarekar@gmail.com>
2026-05-13 16:38:22 +0530

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,43 @@
+export default function Sidebar({ setPage }) {
+
+  return (
+    <div style={{
+      width: '250px',
+      background: '#111',
+      padding: '20px'
+    }}>
+
+      <h1 style={{
+        color: '#d4af37'
+      }}>
+        Trading Journal Guru
+      </h1>
+
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+        marginTop: '30px'
+      }}>
+
+        <button onClick={() => setPage('dashboard')}>
+          Dashboard
+        </button>
+
+        <button onClick={() => setPage('add')}>
+          Add Trade
+        </button>
+
+        <button onClick={() => setPage('journal')}>
+          Journal
+        </button>
+
+        <button onClick={() => setPage('login')}>
+          Login
+        </button>
+
+      </div>
+
+    </div>
+  )
+}
